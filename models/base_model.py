bf93f3ef12de673020f7c253f41a52ae42741d64
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-02-08 11:10:18 +0000

    updated documentation in part

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+""" This module defines a class `BaseModel` """
+
+
+class BaseModel():
+    """ defines all common attributes/methods for other classes """
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of BaseModel
+
+        Args:
+            *args (any): unused
+            **kwargs (dict): key & value pair of attributes
+        """
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue;
+                elif key == "updated_at" or key == "created_at":
+                    self.__dict__[key] = datetime.fromisoformat(value);
+                else:
+                    self.__dict__[key] = value;
+        else:  
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
