f092f65a786d11fe7e1a12e9b9a6684dc9516bc4
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-02-08 11:39:06 +0000

    update to pass pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -18,12 +18,12 @@ class BaseModel():
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "__class__":
-                    continue;
+                    continue
                 elif key == "updated_at" or key == "created_at":
-                    self.__dict__[key] = datetime.fromisoformat(value);
+                    self.__dict__[key] = datetime.fromisoformat(value)
                 else:
-                    self.__dict__[key] = value;
-        else:  
+                    self.__dict__[key] = value
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -34,12 +34,16 @@ class BaseModel():
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ updates the public instance attribute updated_at with the current datetime """
+        """ updates the public instance attribute `updated_at`
+            with the current datetime
+        """
 
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """  returns a dictionary containing all keys/values of __dict__ of the instance """
+        """ returns a dictionary containing all
+            keys/values of `__dict__` of the instance
+        """
 
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
