67eee3a3f4f42ce9774c0a4b46f31680a43f726f
stella <stellaugochukwu100@gmail.com>
2023-02-08 12:29:35 +0100

    updated with more documentation

--- models/base_model.py
+++ models/base_model.py
@@ -29,12 +29,18 @@ class BaseModel():
             self.updated_at = datetime.now()
 
     def __str__(self):
+        """ Defines the string representation of the instance """
+
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """ updates the public instance attribute updated_at with the current datetime """
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """  returns a dictionary containing all keys/values of __dict__ of the instance """
+
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
         dict_copy["created_at"] = self.created_at.isoformat()
