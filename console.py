9bf34c9e3a6febff891663de5dfffbbbc9b266d3
stella <stellaugochukwu100@gmail.com>
2023-02-12 13:46:56 +0100

    documentation update for default command

--- console.py
+++ console.py
@@ -198,6 +198,12 @@ class HBNBCommand(cmd.Cmd):
             elif args[1].startswith("destroy"):
                 id_ = args[1].split('"')[1]
                 self.do_destroy(f"{args[0]} {id_}")
+            elif args[1].startswith("update"):
+                split_ = re.split('"update\("|", "|\"', args[1])
+                id_ = split_[1]
+                attr_name = split_[2]
+                attr_value = split_[3]
+                self.do_update(f'{args[0]} {id_} {attr_name} "{attr_value}"')
 
 
 if __name__ == '__main__':
